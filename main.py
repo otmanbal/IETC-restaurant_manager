92ce52a02de108a7933876ee8407094870a1e97d
BrokeAnas <anas2b@outlook.com>
2025-06-09 18:49:48 +0200

    Mise à jour du fichier main.py qui permet simplement de lancer l'application
    Cette vue reprends toutes les vues/fichier requis afin de pouvoir lancer l'application

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import sys
+from PySide6.QtWidgets import QApplication
+
+from controllers.TableController import TableController
+from views.financeView import FinanceView
+from views.loginView import LoginPage
+from views.mainWindow import mainWindow
+from views.tableView import TableView
+
+
+def main():
+    app = QApplication(sys.argv)
+    
+    login = LoginPage()
+    login.show()
+
+    def handle_login(username, is_admin):
+        login.close()
+        main_window = mainWindow()
+        main_window.show()
+        
+    login.login_successful.connect(handle_login)
+    
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+
+    main()
 

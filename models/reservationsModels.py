7162242f3ed441b4e5de69bd33483454a483a7bd
BrokeAnas <anas2b@outlook.com>
2025-06-10 15:01:29 +0200

    [ADD] Ajout d'un moyen pour charger les réservations dans reservationsModels.py

--- models/reservationsModels.py
+++ models/reservationsModels.py
@@ -1,1 +1,9 @@
+import json
+from collections import defaultdict
 
+class ReservationModel:
+    @staticmethod
+    def load_reservations(json_path):
+        """Charge les réservations depuis un fichier JSON."""
+        with open(json_path, "r", encoding="utf-8") as f:
+            return json.load(f)

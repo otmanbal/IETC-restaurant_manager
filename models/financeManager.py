f343fc9fdb09d8a9bc0a8f750ca51fc7d6bedba0
BrokeAnas <anas2b@outlook.com>
2025-06-12 14:32:34 +0200

    [ADD]-Ajout d'une fonction dans financeManager.py
    Ajout d'une fonction load_finances() qui va permettre de charger les données financières depuis un fichier JSON

--- models/financeManager.py
+++ models/financeManager.py
@@ -1,1 +1,15 @@
+import json
+from datetime import datetime
 
+FINANCE_FILE = "data/finances.json"
+
+def load_finances():
+    """
+    Charge les données financières depuis le fichier JSON spécifié et retourne
+    une liste de transactions si le fichier existe, sinon une liste vide.
+    """
+    try:
+        with open(FINANCE_FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []

e16d35fb34d6c7ccd29bd42ee4bf6c02ecd176c1
mickbron <87870463+mickbron@users.noreply.github.com>
2025-06-15 18:24:31 +0200

    [ADD] - gestionFinances - Ajout de la fonction ajouterEntree() Vérifie que le type_paiement est bien soit "virement" soit "cache".

--- controllers/gestionFinances.py
+++ controllers/gestionFinances.py
@@ -24,3 +24,28 @@ def sauvegarderDonnees(donnees):
     """
     with open(FICHIER_JSON, 'w', encoding='utf-8') as fichier:
         json.dump(donnees, fichier, indent = 4, ensure_ascii = False)
+
+
+def ajouterEntree(id, date, type_paiement, total_facture):
+    """
+        Vérifie que le type_paiement est bien soit "virement" soit "cache". Sinon, déclenche une erreur.
+        Charge les données existantes du fichier JSON.
+        Crée un dictionnaire représentant une nouvelle entrée de paiement
+        Ajoute cette nouvelle entrée à la liste existante avec append().
+        Sauvegarde la nouvelle liste dans le fichier JSON.
+        Affiche un message de confirmation dans la console.
+    """
+    if type_paiement not in ["virement", "cache"]:
+        raise ValueError("Type de paiement invalide. Utiliser 'virement' ou 'cache'.")
+    
+    donnees = chargerDonnees()
+    entree = {
+        "id": id,
+        "date": date,
+        "type_paiement": type_paiement,
+        "total_facture": total_facture
+    }
+
+    donnees.append(entree)
+    sauvegarderDonnees(donnees)
+    print("Entrée ajoutée avec succès.")

a50c2ff70598271349d2d06879db6745e5b78a7d
otmanbal <otman.balhor@hainaut-promsoc.be>
2025-06-15 20:36:29 +0200

    [ADD] - Ajout de la vue "tableView" et gestion des tailles de table

--- views/tableView.py
+++ views/tableView.py
@@ -1,1 +1,14 @@
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QGridLayout, QPushButton
+from PySide6.QtCore import Qt
+from controllers.tableController import tableController
 
+TABLE_SIZE_MAP = {2: (120, 60), 4: (120, 80), 6: (120, 200), 8: (120, 270)}
+
+class tableView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Les Saveurs Orientales")
+        self.setGeometry(150, 100, 1150, 650)
+        self.controller = tableController(self)
+        self.buttons = {}
+        self._setup_ui()

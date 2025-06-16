5a8e41046230097da4318a8c74fda323e4d06ce6
hsx1111 <husseinnsakrr@gmail.com>
2025-06-16 14:36:52 +0200

    [ADD] - MAIN - lancement de l'application
    Ajout des fonctionnalités qui appellent les fonction nécessaires au lancement de l'application

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import sys
+from PySide6.QtWidgets import QApplication
 
+from views.loginView import LoginPage
+from views.mainWindow import mainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    
+    login = LoginPage()
+    login.show()
+
+    def handle_login(username, is_admin):
+        login.close()
+        main_window = mainWindow(username=username, is_admin=is_admin)
+        main_window.show()
+        
+    login.login_successful.connect(handle_login)
+    
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
